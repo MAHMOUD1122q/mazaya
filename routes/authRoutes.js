2bb125b6748f64c2a98d7238209fc22141216b33
Mahmoud Mohamed <95969990+MAHMOUD1122q@users.noreply.github.com>
2025-06-03 21:19:50 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { body } from "express-validator";
-import {login, loginLab, logout } from "../controller/authController.js";
+import {login, loginLab, logout ,refreshToken } from "../controller/authController.js";
 // import { authenticate } from "../middlewares/auth.js";
 import { loginLimiter } from "../middlewares/rate-limit.js";
 const router = express.Router();
@@ -9,6 +9,6 @@ const router = express.Router();
 router.post("/login" , loginLimiter, login);
 router.post("/lab-login" , loginLimiter, loginLab);
 router.post("/logout", logout);
-
+router.post("/refresh", refreshToken);
 
 export default router;
