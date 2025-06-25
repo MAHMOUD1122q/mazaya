48ff2723b7e80325566778738f7deb7af2a6d56f
Mahmoud Mohamed <95969990+MAHMOUD1122q@users.noreply.github.com>
2025-06-25 13:01:24 +0300

    Update productController.js

--- controller/productController.js
+++ controller/productController.js
@@ -10,7 +10,7 @@ const generateProductCode = () => {
 export const getByCode = async (req, res) => {
   try {
     const product = await Product.findOne({ code: req.params.code }).select(
-      "-__v -branches -_id -totalQuantity"
+      "-__v -branches -_id -totalQuantity -createdAt -updatedAt "
     );
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
