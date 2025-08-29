ee5ca07140a561221e45ba10b9f48ab94dfe54ac
Mahmoud Mohamed <95969990+MAHMOUD1122q@users.noreply.github.com>
2025-08-29 23:58:54 +0300

    Delete routes directory

--- routes/companyRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { updateCompany } from "../controller/companyController.js";
-const router = express.Router();
-
-router.put("/update", updateCompany);
-
-export default router;
