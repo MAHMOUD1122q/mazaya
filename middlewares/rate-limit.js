4e715aef234ba3dc1e9c89fb3e6f773ffcd5087d
Mahmoud Mohamed <95969990+MAHMOUD1122q@users.noreply.github.com>
2025-10-14 11:18:23 +0300

    Update rate-limit.js

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -11,7 +11,7 @@ import rateLimit from "express-rate-limit";
   // Rate limiting middleware
  export const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // Max 5 requests per windowMs
+    max: 100, // Max 5 requests per windowMs
     message: { error: "Too many requests, please try again later." },
     standardHeaders: true,
     legacyHeaders: false,
@@ -19,7 +19,8 @@ import rateLimit from "express-rate-limit";
   // Using express-rate-limit
  export const otpRequestLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 3, // Max 3 requests per windowMs
+    max: 100, // Max 3 requests per windowMs
     message: { error: "Too many requests, please try again later." },
 
   });
+
